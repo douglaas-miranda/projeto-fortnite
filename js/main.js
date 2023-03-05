4dd9e274b122d76ab22dba92c2db020c7a480814
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-03-05 15:47:00 -0300

    Ajustando versão mobile e fazendo interação menu no JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
 const modos =  document.querySelector('.modos');
 const modosNav = document.querySelector('.modos-nav');
+const menuMobile =  document.querySelector('.menu-li');
+const modosMobile =  document.querySelector('.menu-mobile')
+
+//-----------------------------------------------------------------------
+//Desktop
 
 
 modos.addEventListener("mouseover", function() {
@@ -20,5 +25,30 @@ modosNav.addEventListener("mouseover", function() {
 
 modosNav.addEventListener("mouseout", function() {
     const elemento = document.querySelector('.modos-nav');
+    elemento.style.display = 'none';
+})
+
+
+//-----------------------------------------------------------------------
+//Mobile
+
+menuMobile.addEventListener("click", function() {
+    const elemento = document.querySelector('.menu-mobile');
+    elemento.style.display = 'inline-block';
+})
+
+menuMobile.addEventListener("mouseout", function() {
+    const elemento = document.querySelector('.menu-mobile');
+    elemento.style.display = 'none';
+})
+
+
+modosMobile.addEventListener("mouseover", function() {
+    const elemento = document.querySelector('.menu-mobile');
+    elemento.style.display = 'inline-block';
+})
+
+modosMobile.addEventListener("mouseout", function() {
+    const elemento = document.querySelector('.menu-mobile');
     elemento.style.display = 'none';
 })
